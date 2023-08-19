e2b963a4078563b3d26710bc957a289c552decec
ChrisRDavila <crossdavila@gmail.com>
2023-08-18 21:42:24 -0700

    Logic branches work, will reveal politcal leaning based on scale of 1-10, with or branches for extremist answer and gag moderate result for true  on Biden

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+// User Interface Logic
+
+function hideResultsAndError() {
+  document.getElementById("error-message").setAttribute("class", "hidden");
+  document.getElementById("conservative").setAttribute("class", "hidden");
+  document.getElementById("moderate").setAttribute("class", "hidden");
+  document.getElementById("liberal").setAttribute("class", "hidden");
+  document.getElementById("sorry").setAttribute("class", "hidden");
+}
+
+window.onload = function() {
+  document.querySelector("form").onsubmit = function(event) {
+    event.preventDefault();
+    hideResultsAndError();
+    const questionOne = parseInt(document.querySelector("input#questionOne").value);
+    const questionTwo = parseInt(document.querySelector("input#questionTwo").value);
+    const questionThree = parseInt(document.querySelector("input#questionThree").value);
+    const questionFour = parseInt(document.querySelector("input#questionFour").value);
+    const questionFive = parseInt(document.querySelector("input#questionFive").value);
+
+    let total = questionOne + questionTwo + questionThree + questionFour + questionFive;
+
+    if (!total) {
+      document.getElementById("error-message").removeAttribute("class");
+      // in all other cases, we run our code to see what ride a user can go on
+    } else {
+      if (total <= 3 || questionFour === 0) {
+      document.getElementById("conservative").removeAttribute("class");
+      } else if (total > 3 && total <= 7 || questionThree === 2) {
+      document.getElementById("moderate").removeAttribute("class");
+      } else if (total > 7) {
+      document.getElementById("liberal").removeAttribute("class");
+      } else {
+      document.getElementById("sorry").removeAttribute("class");
+      }
+    };  
+  }
+}
